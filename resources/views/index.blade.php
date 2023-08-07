5ad72645661a06ff114a2a152b3d43e4a9c72d6d
Rodolfo0 <fuzzyrodo@gmail.com>
2023-08-07 15:46:27 -0600

    Navigation bar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,9 @@
 <x-app-layout>
-    inicio
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 mt-14 roun">
+            <div class="w-full h-[50em] rounded-lg bg-green-900">
+    
+            </div>
+        </div>
+    </div>
 </x-app-layout>
