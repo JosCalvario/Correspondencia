a0c700f25828a899cc9b700284044c28779d2590
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-08-09 17:45:56 -0600

    Tabla responsive decente

--- resources/views/web/documents/index.blade.php
+++ resources/views/web/documents/index.blade.php
@@ -184,15 +184,15 @@
          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead class=" uppercase text-gray-800 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
            <tr>
-            <th scope="col" class="px-4 py-3">Nombre de documento</th>
-            <th scope="col" class="px-4 py-3">Tipo de documento</th>
-            <th scope="col" class="px-4 py-3">Fecha</th>
-            <th scope="col" class="px-4 py-3">Número</th>
-            <th scope="col" class="px-4 py-3">Remitente</th>
-            <th scope="col" class="px-4 py-3">Asunto</th>
-            <th scope="col" class="px-4 py-3">Área asignada</th>
-            <th scope="col" class="px-4 py-3">Documento</th>
-            <th scope="col" class="px-4 py-3">
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Nombre de documento</th>
+            <th id="12" scope="col" class="hidden sm:table-cell px-4 py-3">Tipo de documento</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Fecha</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Número</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Remitente</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Asunto</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Área asignada</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Documento</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">
              <span class="sr-only">Actions</span>
             </th>
            </tr>
@@ -202,14 +202,14 @@
            @foreach ($documents as $doc)
 
            <tr class="border-b text-sm text-gray-900 font-medium">
-            <td id="12" data-label="Nombre de documento" scope="row" class=" before:content-[attr(data-label)] text-left  before:mb-5  sm:before:content-none px-4 py-3 text-xs sm:table-cell block before:block" >{{$doc->name}}</td>
-            <td data-label="Tipo de documento" class=" before:content-[attr(data-label)] text-left  before:mb-5  sm:before:content-none  px-4 py-3 sm:table-cell block before:block">{{$doc->doc_type->name}}</td>
-            <td data-label="Fecha" class=" before:content-[attr(data-label)] text-left  before:mb-5  sm:before:content-none px-4 py-3 sm:table-cell block before:block">{{$doc->date}}</td>
-            <td data-label="Número" class=" before:content-[attr(data-label)] text-left  before:mb-5   sm:before:content-none px-4 py-3 sm:table-cell block before:block">{{$doc->number}}</td>
-            <td data-label="Remitente" class=" before:content-[attr(data-label)] text-left  before:mb-5   sm:before:content-none px-4 py-3 sm:table-cell block before:block">{{$doc->sender}}</td>
-            <td data-label="Asunto" class=" before:content-[attr(data-label)] text-left  before:mb-5   sm:before:content-none px-4 py-3 sm:table-cell block before:block">{{$doc->subject}}</td>
-            <td data-label="Área asignada" class=" before:content-[attr(data-label)] text-left  before:mb-5 sm:before:content-none px-4 py-3 sm:table-cell block before:block">{{$doc->area->name}}</td>
-            <td data-label="Documento" class=" before:content-[attr(data-label)] text-left  before:mb-5  sm:before:content-none px-4 py-3 sm:table-cell block before:block" align="center"><a href="{{asset('storage/documents/'.$doc->document)}}" target="_BLANK">
+            <td data-label="Nombre de documento" scope="row" class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none px-4 py-3 text-xs sm:table-cell block before:block before:font-semibold" >{{$doc->name}}</td>
+            <td data-label="Tipo de documento" class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none  px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->doc_type->name}}</td>
+            <td data-label="Fecha" class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->date}}</td>
+            <td data-label="Número" class=" before:content-[attr(data-label)] text-left  before:mb-2   sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->number}}</td>
+            <td data-label="Remitente" class=" before:content-[attr(data-label)] text-left  before:mb-2   sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->sender}}</td>
+            <td data-label="Asunto" class=" before:content-[attr(data-label)] text-left  before:mb-2   sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->subject}}</td>
+            <td data-label="Área asignada" class=" before:content-[attr(data-label)] text-left  before:mb-2 sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->area->name}}</td>
+            <td data-label="Documento" class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold" align="center"><a href="{{asset('storage/documents/'.$doc->document)}}" target="_BLANK">
              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
             </svg></a></td>
