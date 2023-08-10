48ce5a1dfe3e31250be168a9b45d0f4060d48cbc
UnclearBag1619 <60374342+UnclearBag1619@users.noreply.github.com>
2023-08-09 18:44:09 -0600

    Show modal para documentos

--- resources/views/web/documents/index.blade.php
+++ resources/views/web/documents/index.blade.php
@@ -192,6 +192,7 @@
             <th scope="col" class="hidden sm:table-cell px-4 py-3">Asunto</th>
             <th scope="col" class="hidden sm:table-cell px-4 py-3">Área asignada</th>
             <th scope="col" class="hidden sm:table-cell px-4 py-3">Documento</th>
+            <th scope="col" class="hidden sm:table-cell px-4 py-3">Contestación</th>
             <th scope="col" class="hidden sm:table-cell px-4 py-3">
              <span class="sr-only">Actions</span>
             </th>
@@ -209,13 +210,17 @@
             <td data-label="Remitente" class=" before:content-[attr(data-label)] text-left  before:mb-2   sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->sender}}</td>
             <td data-label="Asunto" class=" before:content-[attr(data-label)] text-left  before:mb-2   sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->subject}}</td>
             <td data-label="Área asignada" class=" before:content-[attr(data-label)] text-left  before:mb-2 sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold">{{$doc->area->name}}</td>
+            @if ($doc->document != '')
             <td data-label="Documento" class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none px-4 py-3 sm:table-cell block before:block before:font-semibold" align="center"><a href="{{asset('storage/documents/'.$doc->document)}}" target="_BLANK">
-             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
-            </svg></a></td>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-pdf" viewBox="0 0 16 16">
+                 <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2h-1v-1h1a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM1.6 11.85H0v3.999h.791v-1.342h.803c.287 0 .531-.057.732-.173.203-.117.358-.275.463-.474a1.42 1.42 0 0 0 .161-.677c0-.25-.053-.476-.158-.677a1.176 1.176 0 0 0-.46-.477c-.2-.12-.443-.179-.732-.179Zm.545 1.333a.795.795 0 0 1-.085.38.574.574 0 0 1-.238.241.794.794 0 0 1-.375.082H.788V12.48h.66c.218 0 .389.06.512.181.123.122.185.296.185.522Zm1.217-1.333v3.999h1.46c.401 0 .734-.08.998-.237a1.45 1.45 0 0 0 .595-.689c.13-.3.196-.662.196-1.084 0-.42-.065-.778-.196-1.075a1.426 1.426 0 0 0-.589-.68c-.264-.156-.599-.234-1.005-.234H3.362Zm.791.645h.563c.248 0 .45.05.609.152a.89.89 0 0 1 .354.454c.079.201.118.452.118.753a2.3 2.3 0 0 1-.068.592 1.14 1.14 0 0 1-.196.422.8.8 0 0 1-.334.252 1.298 1.298 0 0 1-.483.082h-.563v-2.707Zm3.743 1.763v1.591h-.79V11.85h2.548v.653H7.896v1.117h1.606v.638H7.896Z"/>
+               </svg></a></td>
+                @else
+                <td></td>
+            @endif
 
             <td class="px-4 py-3 flex items-center justify-end">
-             <button id="apple-imac-27-dropdown-button" data-modal-toggle="apple-imac-27-dropdown"
+             <button data-modal-toggle="showDocument{{$doc->id}}"
               class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
               type="button">
               <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
@@ -223,8 +228,52 @@
                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
               </svg>
              </button>
-             {{-- Show Modal --}}
-             <div></div>
+             <div id="showDocument{{$doc->id}}" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-full">
+                <div class="relative p-4 w-full max-w-xl h-full md:h-auto">
+                    <!-- Modal content -->
+                    <div class="relative p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+                            <!-- Modal header -->
+                            <div class="flex justify-between mb-4 rounded-t sm:mb-5">
+                                <div class="text-lg text-gray-900 md:text-xl dark:text-white">
+                                    <h3 class="font-semibold ">
+                                        Acuse
+                                    </h3>
+                                    <p class="font-bold">
+                                        {{-- Folio --}}
+                                        Folio: {{$doc->number}}
+                                    </p>
+                                </div>
+                                <div>
+                                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="showDocument{{$doc->id}}">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <dl>
+                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Nombre</dt>
+                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{$doc->name}}</dd>
+                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Fecha</dt>
+                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{$doc->date}}</dd>
+                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Remitente</dt>
+                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{$doc->sender}}</dd>
+                                <dt class="mb-2 font-semibold leading-none text-gray-900 dark:text-white">Asunto</dt>
+                                <dd class="mb-4 font-light text-gray-500 sm:mb-5 dark:text-gray-400">{{$doc->subject}}</dd>
+                            </dl>
+                            <div class="flex justify-between items-center">
+                                <div class="flex items-center space-x-3 sm:space-x-4">
+                                    <button type="button" class="text-white inline-flex items-center bg-sc_greeny hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                        <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
+                                        Responder
+                                    </button>               
+                                    <button type="button" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                                        Previsualizar
+                                    </button>
+                                </div>              
+                            </div>
+                    </div>
+                </div>
+            </div>
    
              {{-- Edit Modal --}}
              <div></div>
