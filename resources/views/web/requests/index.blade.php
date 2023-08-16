4875020c7f36bca759740314682740bb3a4c1ff8
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-08-16 15:50:47 -0600

    Modal funcional

--- resources/views/web/requests/index.blade.php
+++ resources/views/web/requests/index.blade.php
@@ -95,10 +95,11 @@
        @endforeach
        {{-- Create Modal --}}
        @can('requests.create')
-       <div id="createModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-scroll overflow-x-hidden fixed m-auto top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-modal md:h-auto">
+       <div id="createModal" tabindex="-1" aria-hidden="true" class="hidden overflow-x-hidden fixed m-auto top-0 right-0 left-0 z-50 justify-center items-center w-full md:h-auto ">
+        {{-- overflow-x-hidden fixed m-auto top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen --}}
            <div class="relative p-4 w-full max-w-3xl h-full md:h-auto">
                <!-- Modal content -->
-               <div class="relative m-auto p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5">
+               <div class="relative m-auto p-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:p-5 overflow-y-hidden h-[calc(100vh-2rem)]">
                    <!-- Modal header -->
                    <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
@@ -110,10 +111,10 @@
                        </button>
                    </div>
                    <!-- Modal body -->
-                   <form action="{{route('requests.store')}}" method="POST" enctype="multipart/form-data">
+                   <form action="{{route('requests.store')}}" method="POST" enctype="multipart/form-data" class="h-full">
                     @csrf
                     @method('POST')
-                       <div class="grid gap-2 mb-4 sm:grid-cols-2">
+                       <div class="grid gap-2 mb-4 sm:grid-cols-2 overflow-y-scroll h-[calc(100%-8rem)] relative">
                         <div>
                             <label for="folio" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Folio asignado</label>
                             <input type="text" name="folio" id="folio" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" readonly value="{{$folio}}">
@@ -180,7 +181,7 @@
                            </div>
 
                        </div>
-                           <div class="flex items-center sm:justify-left relative -bottom-5 h-9">
+                           <div class="flex items-center sm:justify-end relative text-center">
                                 <button type="submit" class="text-white inline-flex items-center bg-sc_greeny hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600  dark:hover:bg-primary-700 sm:w-auto w-full dark:focus:ring-primary-800">
                                    <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
                                    Agregar
