fef25013cedd1a262d96910303e83ccdd65fee99
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-09-01 16:10:39 -0600

    Componentes tabla completos

--- resources/views/components/web/table.blade.php
+++ resources/views/components/web/table.blade.php
@@ -1,16 +1,15 @@
 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-    <thead class=" uppercase text-gray-800 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
-     <tr>
-        @foreach(explode('|', $headers) as $header)
-      <th scope="col" class="hidden sm:table-cell px-2 py-3">{{$header}}</th>
-        @endforeach
-       <span class="sr-only">Acciones</span>
-      </th>
-     </tr>
-    </thead>
-    <tbody>
-        {{$data}}
+ <thead class=" uppercase text-gray-800 bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+  <tr>
+   @foreach (explode('|', $headers) as $header)
+    <th scope="col" class="hidden sm:table-cell px-2 py-3">{{ $header }}</th>
+   @endforeach  
+   </th>
+  </tr>
+ </thead>
+ <tbody>
+  {{ $data }}
 
 
-    </tbody>
-   </table>
+ </tbody>
+</table>
