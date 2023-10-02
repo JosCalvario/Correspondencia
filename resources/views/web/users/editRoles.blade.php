b5aa6d38e90051b3384a7bb5c606a9eb73314546
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-10-01 18:36:05 -0600

    Vista detalle base

--- resources/views/web/users/editRoles.blade.php
+++ resources/views/web/users/editRoles.blade.php
@@ -8,7 +8,7 @@
     <!-- Modal header -->
     <div class="flex justify-start items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
      <button type="button"
-      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+      class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 mr-2 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
       <i class="bi bi-chevron-left"></i>
       <span class="sr-only">Close modal</span>
      </button>
@@ -18,7 +18,7 @@
 
     </div>
     <!-- Modal body -->
-    <form action="{{ route('users.updateRoles') }}" method="POST">
+    <form class=" w-full h-[calc(100%-5rem)]" action="{{ route('users.updateRoles') }}" method="POST">
      @csrf
      @method('PUT')
      <input type="hidden" name="user_id" value="{{ $user->id }}">
@@ -27,7 +27,7 @@
       <ul
        class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
        @foreach ($roles as $role)
-        <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600">
+        <li class="w-full border-b border-gray-200 rounded-t-lg dark:border-gray-600 relative">
          <div class="flex items-center pl-3">
           <input type="checkbox" name="roles[]" value="{{ $role->id }}"
            {{ $user->hasRole($role->name) ? 'checked' : '' }}
@@ -36,10 +36,11 @@
            class="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ $role->name }}</label>
          </div>
         </li>
+        
        @endforeach
       </ul>
      </div>
-     <div class="flex items-center space-x-4">
+     <div class="flex items-center space-x-4 p-5 justify-end m-0">
       <button type="submit"
        class="text-white bg-sc_greeny hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
        Actualizar
