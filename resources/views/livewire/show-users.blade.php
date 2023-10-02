06aaedda28fac58d3a4f37871296ff8d970faff6
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-10-02 14:57:29 -0600

    details

--- resources/views/livewire/show-users.blade.php
+++ resources/views/livewire/show-users.blade.php
@@ -1,35 +1,36 @@
 <x-web.container title="Usuarios" search="true" actions="false" filters="true" add="true">
 
-    <x-slot name="searchInput">
-        <x-web.searchInput placeholder="Nombre documento, número o asunto" :optionsModel="$options" optionModel="option" searchModel="search"></x-web.searchInput>
-    </x-slot>
-    
-    <x-slot name="addbutton">
-        @can('users.store')
-            <x-web.addTableButton createModalId="createModal">Agregar usuario</x-web.addTableButton>
-        @endcan
-    </x-slot>
-    
-    <x-slot name="actionsSelect">
-        <x-web.actionsSelect dropDownId="actionsDropdown">
-        <x-slot name="actions">
-        <x-web.actionAnchor actionModel="xd">acción 1</x-web.actionAnchor>
-        </x-slot>
-    
-        <x-slot name="mainAction">
-        <x-web.mainActionAnchor actionModel="xd">acción principal</x-web.mainActionAnchor>
-        </x-slot>
-        </x-web.actionsSelect>
-    </x-slot>
-    
-    <x-slot name="filtersSelect">
-        <x-web.filtersSelect dropDownId="filterDropdown">
-        <x-slot name="filterOptions">
-        <x-web.filterOption filterModel="xd" value="0">filtro 1</x-web.filterOption>
-        </x-slot>
-        </x-web.filtersSelect>
-    </x-slot>
-    <x-slot name="Table">
+ <x-slot name="searchInput">
+  <x-web.searchInput placeholder="Nombre documento, número o asunto" :optionsModel="$options" optionModel="option"
+   searchModel="search"></x-web.searchInput>
+ </x-slot>
+
+ <x-slot name="addbutton">
+  @can('users.store')
+   <x-web.addTableButton createModalId="createModal">Agregar usuario</x-web.addTableButton>
+  @endcan
+ </x-slot>
+
+ <x-slot name="actionsSelect">
+  <x-web.actionsSelect dropDownId="actionsDropdown">
+   <x-slot name="actions">
+    <x-web.actionAnchor actionModel="xd">acción 1</x-web.actionAnchor>
+   </x-slot>
+
+   <x-slot name="mainAction">
+    <x-web.mainActionAnchor actionModel="xd">acción principal</x-web.mainActionAnchor>
+   </x-slot>
+  </x-web.actionsSelect>
+ </x-slot>
+
+ <x-slot name="filtersSelect">
+  <x-web.filtersSelect dropDownId="filterDropdown">
+   <x-slot name="filterOptions">
+    <x-web.filterOption filterModel="xd" value="0">filtro 1</x-web.filterOption>
+   </x-slot>
+  </x-web.filtersSelect>
+ </x-slot>
+ <x-slot name="Table">
   <x-web.table headers="Id|Nombre|Correo|Area asignada|Roles">
    <x-slot name="data">
     @foreach ($users as $user)
@@ -41,51 +42,32 @@
        <x-web.tableData>{{ $user->email }}</x-web.tableData>
        <x-web.tableData>{{ $user->area != null ? $user->area->name : '' }}</x-web.tableData>
        @can('permissions.update')
-           <td data-label="Fecha" class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none px-0 py-3 sm:table-cell block before:block before:font-semibold"><a href="{{route('users.editRoles',$user->id)}}" type="button" class="px-3 py-2 text-sm font-medium text-center text-white bg-sc_greeny rounded-lg hover:bg-sc_greener focus:ring-4 focus:outline-none hover:cursor-pointer">Editar</a></td>
-        @endcan
+        <td data-label="Fecha"
+         class=" before:content-[attr(data-label)] text-left  before:mb-2  sm:before:content-none px-0 py-3 sm:table-cell block before:block before:font-semibold">
+         <a href="{{ route('users.editRoles', $user->id) }}" type="button"
+          class="px-3 py-2 text-sm font-medium text-center text-white bg-sc_greeny rounded-lg hover:bg-sc_greener focus:ring-4 focus:outline-none hover:cursor-pointer">Editar</a>
+        </td>
+       @endcan
+
+       <a href="{{ route('users.edit', $user->id) }}"
+        class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
+        type="button">
+        <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+         <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+        </svg>
+       </a>
 
       </x-slot>
 
       <x-slot name="extraModal">
-        
+
       </x-slot>
 
       <x-slot name="detailModal">
-       <x-web.detailModal-sm toggleId="showModal{{ $user->id }}">
-        <x-slot name="modalTitle">
-         Usuario
-        </x-slot>
-        <x-slot name="dataList">
-         <x-web.dataTerm>Nombre</x-web.dataTerm>
-         <x-web.dataDescription>{{ $user->name }}</x-web.dataDescription>
-
-         <x-web.dataTerm>Email</x-web.dataTerm>
-         <x-web.dataDescription>{{ $user->email }}</x-web.dataDescription>
-
-         <x-web.dataTerm>Area asignada</x-web.dataTerm>
-         <x-web.dataDescription>{{ $user->area != null ? $user->area->name : '' }}</x-web.dataDescription>
-
-        </x-slot>
-
-        <x-slot name="modalActions">
-
-         <x-web.modalAnchor>
-          <x-slot name="icon">
-           <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-            <path fill-rule="evenodd"
-             d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-             clip-rule="evenodd"></path>
-           </svg>
-          </x-slot>
-          Editar
-         </x-web.modalAnchor>
-        </x-slot>
-       </x-web.detailModal-sm>
+       
       </x-slot>
 
-      
+
 
      </x-web.tableRow>
     @endforeach
@@ -100,7 +82,7 @@
      @csrf
      @method('POST')
      <div class="grid gap-2 mb-4 sm:grid-cols-2 overflow-y-auto h-[calc(100%-8rem)] content-start relative p-1">
-     
+
       <div class="sm:col-span-2">
        <x-web.formLabel for="name">Nombre</x-web.formLabel>
        <x-web.formInput type="text" name="name" id="name" required="true">
@@ -112,17 +94,17 @@
        </x-web.formInput>
       </div>
       <div>
-        <x-web.formLabel for="area_id">Departamento</x-web.formLabel>
-        <x-web.formInput type="select" name="area_id" id="area_id" required="true"
-         placeholder="Asigna un departamento">
- 
-         <x-slot name="options">
-          @foreach ($areas as $area)
-           <option value="{{ $area->id }}">{{ $area->name }}</option>
-          @endforeach
-         </x-slot>
-        </x-web.formInput>
-       </div>
+       <x-web.formLabel for="area_id">Departamento</x-web.formLabel>
+       <x-web.formInput type="select" name="area_id" id="area_id" required="true"
+        placeholder="Asigna un departamento">
+
+        <x-slot name="options">
+         @foreach ($areas as $area)
+          <option value="{{ $area->id }}">{{ $area->name }}</option>
+         @endforeach
+        </x-slot>
+       </x-web.formInput>
+      </div>
       <div>
        <x-web.formLabel for="password">Contraseña</x-web.formLabel>
        <x-web.formInput type="password" name="password" id="password" required="true">
@@ -140,8 +122,8 @@
        class="text-white inline-flex items-center bg-sc_greeny hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600  dark:hover:bg-primary-700 sm:w-auto w-full dark:focus:ring-primary-800">
        <svg class="mr-1 -ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd"
-         d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-         clip-rule="evenodd"></path>
+         d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd">
+        </path>
        </svg>
        Agregar
       </button>
