a374688538f4d565626f468e1bf7f81f1b2dade6
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2023-10-27 15:20:05 -0600

    table css

--- resources/views/reports/closing.blade.php
+++ resources/views/reports/closing.blade.php
@@ -7,91 +7,96 @@
  <title>Sistema de correspondencia</title>
 
  <style>
+  .table-container {
+   position: relative;
+   overflow-x: auto;
+   box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+   border-radius: 0.5rem;
+  }
 
+  .table {
+   width: 100%;
+   font-size: 14px;
+   text-align: left;
+   color: rgb(107, 114, 128);
+
+  }
+
+  .table__header {
+   font-size: 12px;
+   color: rgb(55, 65, 81);
+   text-transform: uppercase;
+   background-color: rgb(249, 250, 251);
+
+  }
+
+  .table__header-head {
+   padding: 12px 24px;
+  }
+
+  .table__row {
+   background-color: white;
+   border-bottom-width: 1px;
+  }
+
+  .table__row-head {
+   padding: 16px 24px;
+   font-weight: 500;
+   color: rgb(17, 24, 39);
+   white-space: nowrap;
+  }
+
+  .table__row-data {
+   padding: 16px 24px;
+   
+  }
  </style>
 </head>
 
 <body class="font-sans antialiased">
-    <main>
-        
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Product name
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Color
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only">Edit</span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="px-6 py-4">
-                    Silver
-                </td>
-                <td class="px-6 py-4">
-                    Laptop
-                </td>
-                <td class="px-6 py-4">
-                    $2999
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Microsoft Surface Pro
-                </th>
-                <td class="px-6 py-4">
-                    White
-                </td>
-                <td class="px-6 py-4">
-                    Laptop PC
-                </td>
-                <td class="px-6 py-4">
-                    $1999
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    Magic Mouse 2
-                </th>
-                <td class="px-6 py-4">
-                    Black
-                </td>
-                <td class="px-6 py-4">
-                    Accessories
-                </td>
-                <td class="px-6 py-4">
-                    $99
-                </td>
-                <td class="px-6 py-4 text-right">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+ <main>
 
-    </main>
+  <div class="table-container">
+   <table class="table">
+    <thead class="table__header">
+     <tr>
+      <th scope="col" class="table__header-head">
+       Product name
+      </th>
+      <th scope="col" class="table__header-head">
+       Color
+      </th>
+      <th scope="col" class="table__header-head">
+       Category
+      </th>
+      <th scope="col" class="table__header-head">
+       Price
+      </th>
+      <th scope="col" class="table__header-head">
+       <span class="sr-only">Edit</span>
+      </th>
+     </tr>
+    </thead>
+    <tbody>
+     <tr class="table__row">
+      <th scope="row" class="table__row-head">
+       Apple MacBook Pro 17"
+      </th>
+      <td class="table__row-data">
+       Silver
+      </td>
+      <td class="table__row-data">
+       Laptop
+      </td>
+      <td class="table__row-data">
+       $2999
+      </td>
+     </tr>
+    </tbody>
+   </table>
+  </div>
+
+ </main>
 
 </body>
 
